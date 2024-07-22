7314abe2ad10aea2cc8e139e9ed2591a0715e7b3
Ivaylo Ivanov <ivo.ivanov@null.net>
2024-07-22 13:38:16 +0300

    soc: samsung: exynos-chipid: add exynos8895 SoC support
    Add EXYNOS8895 information to soc_ids tables. This SoC product id
    is "0xE8895000".
    
    Signed-off-by: Ivaylo Ivanov <ivo.ivanov.ivanov1@gmail.com>

--- drivers/soc/samsung/exynos-chipid.c
+++ drivers/soc/samsung/exynos-chipid.c
@@ -57,6 +57,7 @@ static const struct exynos_soc_id {
 	{ "EXYNOS7420", 0xE7420000 },
 	/* Compatible with: samsung,exynos850-chipid */
 	{ "EXYNOS7885", 0xE7885000 },
+	{ "EXYNOS8895", 0xE8895000 },
 	{ "EXYNOS850", 0xE3830000 },
 	{ "EXYNOSAUTOV9", 0xAAA80000 },
 	{ "EXYNOSAUTOV920", 0x0A920000 },
