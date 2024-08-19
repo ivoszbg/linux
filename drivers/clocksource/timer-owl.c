0b2dea5128f99dc42497608ea2ec3dfb44083a03
Ivaylo Ivanov <ivo.ivanov.ivanov1@gmail.com>
2024-08-19 15:08:42 +0300

    clocksource/drivers/owl: Add the ATM7051 timer
    Actions ATM7051 SoC has functionally the same timer block as S500,
    which has two 2Hz timers and two 32-bit timers (TIMER0/1). Declare
    the appropriate compatible to allow using them.
    
    Signed-off-by: Ivaylo Ivanov <ivo.ivanov.ivanov1@gmail.com>

--- drivers/clocksource/timer-owl.c
+++ drivers/clocksource/timer-owl.c
@@ -171,6 +171,7 @@ static int __init owl_timer_init(struct device_node *node)
 
 	return 0;
 }
+TIMER_OF_DECLARE(owl_atm7051, "actions,atm7051-timer", owl_timer_init);
 TIMER_OF_DECLARE(owl_s500, "actions,s500-timer", owl_timer_init);
 TIMER_OF_DECLARE(owl_s700, "actions,s700-timer", owl_timer_init);
 TIMER_OF_DECLARE(owl_s900, "actions,s900-timer", owl_timer_init);
