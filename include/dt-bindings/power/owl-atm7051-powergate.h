9ef7d38d09c031c1c371b0b44cdb2c84ee2cc247
Ivaylo Ivanov <ivo.ivanov.ivanov1@gmail.com>
2024-08-20 10:11:55 +0300

    dt-bindings: power: Add Actions Semi ATM7051 SPS
    Define power domains for Actions Semi ATM7051 SoC Smart Power
    System (SPS).
    
    Signed-off-by: Ivaylo Ivanov <ivo.ivanov.ivanov1@gmail.com>

---
+++ include/dt-bindings/power/owl-atm7051-powergate.h
@@ -0,0 +1,16 @@
+/* SPDX-License-Identifier: (GPL-2.0-or-later OR MIT) */
+/*
+ * Actions Semi ATM7051 SPS
+ *
+ * Copyright (c) 2024 Ivaylo Ivanov <ivo.ivanov.ivanov1@gmail.com>
+ */
+#ifndef DT_BINDINGS_POWER_OWL_ATM7051_POWERGATE_H
+#define DT_BINDINGS_POWER_OWL_ATM7051_POWERGATE_H
+
+#define ATM7051_PD_CPU2		0
+#define ATM7051_PD_CPU3		1
+#define ATM7051_PD_GPU		2
+#define ATM7051_PD_VDE		3
+#define ATM7051_PD_VCE		4
+
+#endif
